8b34c56d8bfcfc7d3976b3d6b530b2e117e2b314
leonardung <leonard.ung@hotmail.com>
2026-01-05 13:57:17 +0100

    allow external connection

--- image_labeling_backend/settings.py
+++ image_labeling_backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-$t!sq+emv804njf8n4ir#*%&cd-w%=479(&@m4ehjy)ydg2=ex
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 
 
 # Application definition
